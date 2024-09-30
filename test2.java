c3b557ede8973da6cb76b56191b8175fbff28c20
haritha-2006 <thah1208@gmail.com>
2024-09-30 10:38:50 +0530

    created file

---
+++ test2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class test2 {
+    public static void main(String[] args) {
+
+        Scanner S = new Scanner(System.in);
+        String RBC = S.nextLine();
+    
+        if(RBC.equals("win")){
+            System.out.println("RCB wins");
+        }
+        else{
+            System.out.println("RCB lose");
+        }
+    }
+}
