0d27725a375457b75d538c8d94deefc93fcead6b
haritha-2006 <thah1208@gmail.com>
2024-10-04 04:53:01 +0530

    file created

--- arr_2D.java
+++ arr_2D.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
 public class arr_2D {
     public static void main(String[] args) {
         int i,j;
-        int[][] a = {{5,4,6,},{1,2,3},{8,9,6}};
-        for(i=0;i<3;i++)
+        Scanner S = new Scanner(System.in);
+        //int a[][] = {{5,4,6,},{1,2,3},{8,9,6}};
+        System.out.print("Enter no. of element :");
+        int n = S.nextInt();
+        int a[][] = new int[n][n];
+        System.out.println("Enter elements :");
+        for(i=0;i<n;i++)
         {
-            for(j=0;j<3;j++){
-                System.out.println(a[i][j]);
+            for(j=0;j<n;j++){
+                a[i][j]=S.nextInt();
             }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 }
