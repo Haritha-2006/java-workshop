d3f382bb2afd6590f602fc62760e82dd4811efbf
haritha-2006 <thah1208@gmail.com>
2024-10-04 05:15:47 +0530

    added

--- arr_1D.java
+++ arr_1D.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 public class arr_1D {
     public static void main(String[] args) {
         int arr[] = new int[5];
-        Scanner S =new Scanner(System.in);
-        for(int i=0;i<5;i++){
-           arr[i]=S.nextInt();
+        try (Scanner S = new Scanner(System.in)) {
+            for(int i=0;i<5;i++){
+                arr[i]=S.nextInt();
+            }
+            System.out.println("the values are ");
+            for(int i=0;i<5;i++){
+                System.out.println(arr[i]);
+            }
         }
-        System.out.println("the values are ");
-        for(int i=0;i<5;i++){
-            System.out.println(arr[i]);
-         }
     }
 }
